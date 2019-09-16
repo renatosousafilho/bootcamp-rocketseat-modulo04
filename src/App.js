12f67b073f3b07426f95304f08da18b911f5facd
Renato Filho <renatosousafilho@gmail.com>
2019-09-16 13:54:21 -0300

    importando imagem

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './App.css';
 
+import coca from './assets/coca.jpg';
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div>
+      <h1>Hello Rocketseat!</h1>
+      <img width="200" src={coca}></img>
+    </div>
+  )
 }
 
 export default App;
